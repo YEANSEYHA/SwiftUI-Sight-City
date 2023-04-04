ba444a13b737eaf6d5cab98eef9cfea684b99e03
Yean Seyha <yeanseyha7@gmail.com>
2023-04-04 17:53:05 +0700

    Initial Commit

---
+++ city-app/city_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  city_appApp.swift
+//  city-app
+//
+//  Created by Yean Seyha on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct city_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
