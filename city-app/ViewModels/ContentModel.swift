abbb8aa2ca5adf24b8e77744176eb53466401d64
Yean Seyha <yeanseyha7@gmail.com>
2023-04-04 18:37:25 +0700

    Get user locations

--- city-app/ViewModels/ContentModel.swift
+++ city-app/ViewModels/ContentModel.swift
@@ -9,15 +9,51 @@ import Foundation
 import CoreLocation
 
 
-class ContentModel: ObservableObject{
+import Foundation
+import CoreLocation
+
+class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     
     var locationManager = CLLocationManager()
     
-    
-    init() {
-        // Reqeust permission from the user
+    override init() {
+        
+        // Init method of NSObject
+        super.init()
+        
+        // Set content model as the delegate of the location manager
+        locationManager.delegate = self
+        
+        // Request permission from the user
         locationManager.requestWhenInUseAuthorization()
-        // TODO: start geolocating the user
-//        locationManager.stopUpdatingLocation()
+        
     }
+    
+    // MARK: - Location Manager Delegate Methods
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        if locationManager.authorizationStatus == .authorizedAlways ||
+            locationManager.authorizationStatus == .authorizedWhenInUse {
+            
+            // We have permission
+            // Start geolocating the user, after we get permission
+            locationManager.startUpdatingLocation()
+        }
+        else if locationManager.authorizationStatus == .denied {
+            // We don't have permission
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Gives us the location of the user
+        print(locations.first ?? "no location")
+        
+        // Stop requesting the location after we get it once
+        locationManager.stopUpdatingLocation()
+        
+        // TODO: If we have the coordinates of the user, send into Yelp API
+    }
+    
+    
 }
